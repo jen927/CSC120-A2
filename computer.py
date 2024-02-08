3aa64662848c30545888b314f0c661f594981227
jen927 <157835344+jen927@users.noreply.github.com>
2024-02-08 11:08:22 -0500

    added buy method, added extension file

--- computer.py
+++ computer.py
@@ -10,11 +10,19 @@ class Computer:
     price: int
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__(self, description, processor_type, hard_drive_capacity, memory,operating_system, year_made, price):
+    def __init__(self,
+                 description:str,
+                 processor_type:str,
+                 hard_drive_capacity:int,
+                 memory:int,
+                 operating_system:str,
+                 year_made:int,
+                 price:int):
         self.description = description
         self.processor_type = processor_type
         self.hard_drive_capacity = hard_drive_capacity
         self.memory = memory
+        self.operating_system = operating_system
         self.year_made = year_made
         self.price = price
         #pass # You'll remove this when you fill out your constructor
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -4,14 +4,29 @@ class ResaleShop:
 
     # What attributes will it need?
     inventory = [] # Computer objects will go in here
-    
+    newComputer: object
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__(self):
-        pass # You'll remove this when you fill out your constructor
-
+    def __init__(self, inventory:list, newComputer:object):
+        self.inventory = inventory
+        self.newComputer = newComputer
     # What methods will you need?
     def buy(self):
-        pass
+        self.inventory.append(self.newComputer)
+        print(self.inventory)
         # 1. Call computer(..) constructor to create a new computer instance
         # 2. call inventory.append(...) to add the new computer instance to the inventory
+    def sell(self):
+        pass
+    def refurbish(self):
+        pass
+
+#running to check if code works
+def main():
+    newComputer = Computer("Mac Pro (Late 2013)",
+        "3.5 GHc 6-Core Intel Xeon E5",
+        1024, 64,
+        "macOS Big Sur", 2013, 1500)
+    computerOne = ResaleShop(inventory,)
+
+main()
